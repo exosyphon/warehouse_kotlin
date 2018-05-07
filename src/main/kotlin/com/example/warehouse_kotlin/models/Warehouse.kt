29b304abe96c3eddc2817f7bdef092042dff6ecc
exosyphon <exosyphon@yahoo.com>
2018-05-07 10:21:35 -0500

    Make warehouse data class

--- src/main/kotlin/com/example/warehouse_kotlin/models/Warehouse.kt
+++ src/main/kotlin/com/example/warehouse_kotlin/models/Warehouse.kt
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class Warehouse(
+data class Warehouse(
         var name: String = "",
         var address: String = "",
         var city: String = "",
