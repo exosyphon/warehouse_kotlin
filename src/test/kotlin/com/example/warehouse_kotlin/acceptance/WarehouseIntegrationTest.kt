fe175a844e96b195857bda8f3c505af7301bcb36
exosyphon <exosyphon@yahoo.com>
2018-07-08 21:54:36 -0500

    Fixed tests after adding flyway

--- src/test/kotlin/com/example/warehouse_kotlin/acceptance/WarehouseIntegrationTest.kt
+++ src/test/kotlin/com/example/warehouse_kotlin/acceptance/WarehouseIntegrationTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -17,6 +18,7 @@ import testutilities.TestUtils
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class WarehouseIntegrationTest {
 
     @Autowired
