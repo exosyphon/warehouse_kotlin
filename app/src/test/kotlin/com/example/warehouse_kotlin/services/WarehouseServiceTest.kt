320e0b04bfc9aee78b887a1545569b6ecc28d651
exosyphon <exosyphon@yahoo.com>
2018-09-17 21:10:14 -0500

    Add test around LocalDate and LocalTime parsing with JSON

--- app/src/test/kotlin/com/example/warehouse_kotlin/services/WarehouseServiceTest.kt
+++ app/src/test/kotlin/com/example/warehouse_kotlin/services/WarehouseServiceTest.kt
@@ -2,6 +2,9 @@ package com.example.warehouse_kotlin.services
 
 import com.example.warehouse_kotlin.models.Warehouse
 import com.example.warehouse_kotlin.repositories.WarehouseRepository
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -10,6 +13,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import java.time.LocalDate
+import java.time.LocalTime
 
 @ExtendWith(MockitoExtension::class)
 class WarehouseServiceTest {
@@ -25,5 +30,22 @@ class WarehouseServiceTest {
             Mockito.`when`(mockWarehouseRepository.findAll()).thenReturn(warehouses)
             assertEquals(WarehouseService(mockWarehouseRepository).findAll(), warehouses)
         }
+
+        @Test
+        fun `should deserialize an object with local date and local time`() {
+            val mapper = ObjectMapper()
+            mapper.registerModule(JavaTimeModule())
+            val ticket = mapper.readValue("""{ "contact_email": "contact@ema.il", "date_opened": "2017-11-13", "time_opened": "00:04:00.456" }""", Ticket::class.java)
+            assert("$ticket" == "Ticket(contactEmail=contact@ema.il, dateOpened=2017-11-13, timeOpened=00:04:00.456)")
+        }
     }
 }
+
+data class Ticket(
+        @JsonProperty("contact_email")
+        var contactEmail: String? = null,
+        @JsonProperty("date_opened")
+        var dateOpened: LocalDate? = null,
+        @JsonProperty("time_opened")
+        var timeOpened: LocalTime? = null
+)
