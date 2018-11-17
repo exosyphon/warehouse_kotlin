bcc97e6b7b405dbe0ab71245545bf526e7459535
exosyphon <exosyphon@yahoo.com>
2018-11-17 00:37:14 -0600

    Add md-select inside a md-dialog that filters options based on last selected value

--- frontend/src/components/Warehouses.vue
+++ frontend/src/components/Warehouses.vue
@@ -17,38 +17,71 @@
     <md-dialog class="modal" :md-active.sync="showDialog">
           <md-dialog-title>Preferences</md-dialog-title>
 
+          <md-dialog-content>
+            <md-field>
+              <label class="label" for="selectedValue">Test Select</label>
+              <md-select class="select" v-model="selectedValue">
+               <md-option v-for="(option, index) in filteredOptions" :key="index" :value="option" class="option">{{option}}</md-option>
+              </md-select>
+            </md-field>
+          </md-dialog-content>
+
           <md-dialog-actions>
-            <md-button class="md-primary" @click="showDialog = false">Close</md-button>
-            <md-button class="md-primary" @click="showDialog = false">Save</md-button>
+            <md-button class="md-primary" @click="close()">Close</md-button>
+            <md-button class="md-primary" @click="save()">Save</md-button>
           </md-dialog-actions>
         </md-dialog>
 
-    <md-button class="md-primary md-raised" @click="showDialog = true">Show Dialog</md-button>
+    <md-button class="md-primary md-raised show-button" @click="showDialog = true">Show Dialog</md-button>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex'
+import _ from 'lodash'
 
 export default {
   name: "Warehouses",
-  computed: Object.assign(
-    {},
-    {
+  computed: {
       query() {
         return window.location.pathname
-      }
-    },
-    mapGetters({
+      },
+      filteredOptions: function() {
+        if(_.isNull(this.lastSelectedValue)) {
+          return this.options
+        }
+
+        return this.options.filter(option => option !== this.lastSelectedValue)
+      },
+    ...mapGetters({
       warehouses: 'getWarehouses'
     })
-  ),
+  },
   created() {
     this.$store.dispatch('FETCH_WAREHOUSES')
   },
   data: () => ({
-        showDialog: false
-      })
+        showDialog: false,
+        selectedValue: null,
+        lastSelectedValue: null,
+        options: [
+          'First',
+          'Second',
+          'Third'
+        ]
+      }),
+  methods: {
+    save: function() {
+      this.showDialog = false
+      console.log(this.selectedValue)
+      this.lastSelectedValue = this.selectedValue
+      this.selectedValue = null
+    },
+    close: function() {
+      this.showDialog = false
+      this.selectedValue = null
+    }
+  }
 };
 </script>
 
--- frontend/test/components/Warehouses.spec.js
+++ frontend/test/components/Warehouses.spec.js
@@ -2,6 +2,32 @@ import { mount, createLocalVue, RouterLink } from 'vue-test-utils'
 import Warehouses from '../../src/components/warehouses'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
+import VueMaterial from 'vue-material'
+
+global.window.document.createRange = function createRange() {
+  return {
+    setEnd: () => {},
+    setStart: () => {},
+    getBoundingClientRect: () => {
+      return { right: 0 };
+    },
+    getClientRects: () => [],
+    commonAncestorContainer: {
+          nodeName: "BODY",
+          ownerDocument: document,
+        }
+  };
+};
+const { getComputedStyle } = window
+window.getComputedStyle = function getComputedStyleStub(el) {
+	return {
+		...getComputedStyle(el),
+		transitionDelay: '',
+		transitionDuration: '',
+		animationDelay: '',
+		animationDuration: '',
+	}
+}
 
 describe('Warehouses', () => {
   let wrapper
@@ -10,6 +36,7 @@ describe('Warehouses', () => {
     const localVue = createLocalVue()
     localVue.use(Vuex)
     localVue.use(VueRouter)
+    localVue.use(VueMaterial)
 
     const state = {
       warehouses: [{id: 1, name: 'tom'}]
@@ -39,7 +66,47 @@ describe('Warehouses', () => {
     expect(wrapper.vm.$route.path).toEqual("/other-warehouses")
   })
 
-  it('renders the correct markup', () => {
-    expect(wrapper.find('.modal').html()).toContain("Preferences")
+  it('renders the correct markup', (done) => {
+    wrapper.find('.show-button').trigger('click')
+
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.find('.modal').html()).toContain("Preferences")
+      done()
+    })
+  })
+
+  it('should update values in a select inside a dialog', (done) => {
+    wrapper.find('.show-button').trigger('click')
+
+    wrapper.vm.$nextTick(() => {
+      wrapper.find('.md-icon').trigger('click')
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.findAll('.option').length).toEqual(3)
+        expect(wrapper.findAll('.option').at(0).text()).toEqual('First')
+        expect(wrapper.findAll('.option').at(1).text()).toEqual('Second')
+        expect(wrapper.findAll('.option').at(2).text()).toEqual('Third')
+
+        wrapper.findAll('.option').at(0).trigger('click')
+        wrapper.vm.$nextTick(() => {
+          expect(wrapper.vm.$data.selectedValue).toEqual('First')
+          done()
+        })
+      })
+    })
+  })
+
+  it('should filter previously selected item from select options', (done) => {
+    wrapper.vm.$data.lastSelectedValue = 'First'
+
+    wrapper.find('.show-button').trigger('click')
+    wrapper.vm.$nextTick(() => {
+      wrapper.find('.md-icon').trigger('click')
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.findAll('.option').length).toEqual(2)
+        expect(wrapper.findAll('.option').at(0).text()).toEqual('Second')
+        expect(wrapper.findAll('.option').at(1).text()).toEqual('Third')
+        done()
+      })
+    })
   })
 })
